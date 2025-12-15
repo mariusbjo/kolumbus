3453e0905c9aad4aaa261f9c73559294f1947b50
mariusbjo <marius.bjornor@gmail.com>
2025-12-15 07:30:46 +0100

    Update icons.js

--- assets/icons.js
+++ assets/icons.js
@@ -1,1 +1,18 @@
+// assets/icons.js
+
+export const busIcon = L.icon({
+  iconUrl: 'https://cdn-icons-png.flaticon.com/512/61/61231.png',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32]
+});
+
+export function speedIcon(speed) {
+  return L.divIcon({
+    className: 'speed-icon',
+    html: `<div>${Math.round(speed)}</div>`,
+    iconSize: [32, 32],
+    iconAnchor: [16, 16]
+  });
+}
 
