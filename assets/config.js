1b81b5f1db1d91aacd434dbc29d4e4ec9178aa27
iRyfylke <245865974+iRyfylke@users.noreply.github.com>
2025-12-29 08:00:20 +0100

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 // assets/config.js
 export const ENTUR_URL = "https://api.entur.io/realtime/v2/vehicles/graphql";
 export const CLIENT_NAME = "marius-kolumbus-demo";
-export const UPDATE_INTERVAL = 5000; // 5 sekunder
+export const UPDATE_INTERVAL = 2000; // 2 sekunder
 
